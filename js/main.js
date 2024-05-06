01e89bf8149c294bab8b38bc6388ef4764cbb0e1
Marco Vizgarra <marcovizgarra@gmail.com>
2024-05-05 21:48:38 -0300

    Array, ciclo for y funcion .join utilizados

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-alert("Bienvenido a el juego del número secreto!\n\nPara jugar, ingresá el rango entre el que queres que se encuentre el número \n\nTenés 5 intentos para advinar el número\n\nSi estas listo presioná aceptar para ingresar el rango en el que estará el número secreto");
+alert("Bienvenido a el juego del número secreto!\n\nPara jugar, ingresá el rango entre el que queres que se encuentre el número \n\nTenés 10 intentos para advinar el número\n\nSi estas listo presioná aceptar para ingresar el rango en el que estará el número secreto");
 
 let min = 0;
 let max = 0;
@@ -49,6 +49,8 @@ if (min != 0 & max != 0) {
 
     alert("Todo listo para jugar, suerte!!");
 
+const intentosRealizados = []
+
     do {
         tryX = parseInt(prompt("Ingresá el número secreto"));
 
@@ -58,14 +60,25 @@ if (min != 0 & max != 0) {
             alert("No podes ingresar números mayores que el máximo establecido, que es: " + max);
         }else if (isNaN(tryX)) {
             alert("Solamente se pueden ingresar números");
-        }else if (tryCount == 4) {
+        }else if (tryCount == 9) {
+            intentosRealizados.push(tryX);
             alert("Agotaste todos tus intentos, recarga la página para volver a intentarlo, que tengas suerte");
             win = true;
+            let mostrarIntentos = "Los intentos realizados fueron: \n· " + intentosRealizados.join('\n· ') + "\n\n Y el número secreto era: " + secretNumber;
+            alert(mostrarIntentos);
         }else if (tryX != secretNumber) {
-            alert("No adivinaste, intentá de nuevo\n\nTe quedan " + (4 - tryCount) + " intentos");
+            alert("No adivinaste, intentá de nuevo\n\nTe quedan " + (9 - tryCount) + " intentos");
             tryCount++;
+            intentosRealizados.push(tryX);
         }else if (tryX == secretNumber) {
-            alert("Felicidades!!, adivinaste el número secreto");
+            
+            // Ciclo FOR que muestra los intentos realizados tras ganar el juego
+            let intentosAlGanar = "";
+            for (let i=0; i < tryCount; i++) {
+                intentosAlGanar += "· " + " " + intentosRealizados[i] + "\n";
+            };
+            
+            alert("Felicidades!!, adivinaste el número secreto\n\n" + "Te llevó " + tryCount + " intentos hacerlo.\n\n"  + "Tus intentos realizados antes de adivinarlo fueron: \n" + intentosAlGanar);
             win = true;
             let message = "Hace clic en aceptar para celebrar";
             message;
@@ -73,5 +86,5 @@ if (min != 0 & max != 0) {
                 window.location.href = "https://www.youtube.com/watch?v=skVg5FlVKS0";
             }
         }
-    }while (win == false & tryCount <5);
+    }while (win == false & tryCount <10);
 }
